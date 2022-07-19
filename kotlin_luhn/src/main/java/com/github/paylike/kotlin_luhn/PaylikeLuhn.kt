201dadf0ed2331f1b36b48d748f4ea48f0c8f8dd
Laszlo Kocsis <kocsislaci95@gmail.com>
2022-07-19 14:25:52 +0200

    feat(sample): deleted unused things

--- kotlin_luhn/src/main/java/com/github/paylike/kotlin_luhn/PaylikeLuhn.kt
+++ kotlin_luhn/src/main/java/com/github/paylike/kotlin_luhn/PaylikeLuhn.kt
@@ -6,7 +6,7 @@ object PaylikeLuhn {
      */
     fun isValid(cardNumber: String): Boolean {
         return cardNumber.substring(cardNumber.length - 1) ==
-                _calculate(cardNumber.substring(0, cardNumber.length - 1))
+                calculate(cardNumber.substring(0, cardNumber.length - 1))
     }
 
     /**
@@ -14,17 +14,17 @@ object PaylikeLuhn {
      */
     fun calculateCheckDigit(cardNumber: String): String
     {
-        return _calculate(cardNumber.substring(0, cardNumber.length - 1))
+        return calculate(cardNumber.substring(0, cardNumber.length - 1))
     }
 
-    private fun _sumDigits(number: String): Int
+    private fun sumDigits(number: String): Int
     {
         return number.split("")
             .filter { it.isNotEmpty() }
             .fold ("0") { acc, curr -> "${acc.toInt() + curr.toInt()}" }.toInt()
     }
 
-    private fun _calculate(numbers: String): String
+    private fun calculate(numbers: String): String
     {
         val length = numbers.length
         val map: Map<Int, String> =
@@ -38,7 +38,7 @@ object PaylikeLuhn {
             .entries
             .fold(0) { acc, element ->
                     val temp: Int = when ((element.key + length) % 2 == 1) {
-                        true -> _sumDigits("${element.value.toInt() * 2}")
+                        true -> sumDigits("${element.value.toInt() * 2}")
                         false -> element.value.toInt()
                     }
                     return@fold acc + temp
