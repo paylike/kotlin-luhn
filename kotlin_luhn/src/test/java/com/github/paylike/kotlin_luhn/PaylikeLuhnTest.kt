67ad606e53f2bf81f6291cb58b1c79ba30e08c2e
Laszlo Kocsis <kocsislaci95@gmail.com>
2022-09-14 12:33:10 +0200

    chore(*): docs, refactor of tests, added spotless and checked syntax on all, added CI executing assemble on push and PR on Main branch

--- kotlin_luhn/src/test/java/com/github/paylike/kotlin_luhn/PaylikeLuhnTest.kt
+++ kotlin_luhn/src/test/java/com/github/paylike/kotlin_luhn/PaylikeLuhnTest.kt
@@ -1,52 +1,43 @@
 package com.github.paylike.kotlin_luhn
 
-import org.junit.Assert.assertTrue
+import org.junit.Assert.*
 import org.junit.Test
 
 class PaylikeLuhnTest {
     @Test
-    fun test() {
-        val pans: List<String> = listOf(
-            /**
-             * From the ISO standard
-             */
-            "6123451234567893",
+    fun paylikeLuhnTest() {
+        val validNumbers: List<String> =
+            listOf(
+                /** From the ISO standard */
+                "6123451234567893",
 
-            /**
-             * http://www.paypalobjects.com/en_US/vhelp/paypalmanager_help/credit_card_numbers.htm
-             */
-            "4012111111111111",
-            "378282246310005",
-            "371449635398431",
-            "378734493671000",
-            "5610591081018250",
-            "30569309025904",
-            "38520000023237",
-            "6011111111111117",
-            "6011000990139424",
-            "3530111333300000",
-            "3566002020360505",
-            "5555555555554444",
-            "5105105105105100",
-            "4111111111111111",
-            "4012888888881881",
-            "4222222222222",
-            "5019717010103742",
-            "6331101999990016"
-        )
-        val invalid: List<String> = listOf(
-            "76009244561",
-            "4571736004738485"
-        )
-        pans.forEach {
-            val result = PaylikeLuhn.isValid(it)
-            assertTrue(result)
-            val calculatedChecksum = PaylikeLuhn.calculateCheckDigit(it)
-            val givenChecksum = it.substring(it.length - 1)
-            assertTrue(calculatedChecksum == givenChecksum)
+                /**
+                 * http://www.paypalobjects.com/en_US/vhelp/paypalmanager_help/credit_card_numbers.htm
+                 */
+                "4012111111111111",
+                "378282246310005",
+                "371449635398431",
+                "378734493671000",
+                "5610591081018250",
+                "30569309025904",
+                "38520000023237",
+                "6011111111111117",
+                "6011000990139424",
+                "3530111333300000",
+                "3566002020360505",
+                "5555555555554444",
+                "5105105105105100",
+                "4111111111111111",
+                "4012888888881881",
+                "4222222222222",
+                "5019717010103742",
+                "6331101999990016"
+            )
+        val invalidNumbers: List<String> = listOf("76009244561", "4571736004738485")
+        validNumbers.forEach {
+            assertTrue(PaylikeLuhn.isValid(it))
+            assertEquals(it.last().toString(), PaylikeLuhn.calculateCheckDigit(it))
         }
-        invalid.forEach {
-            assertTrue(!PaylikeLuhn.isValid(it))
-        }
+        invalidNumbers.forEach { assertFalse(PaylikeLuhn.isValid(it)) }
     }
 }
